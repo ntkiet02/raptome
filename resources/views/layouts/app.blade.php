0244359534e36bca7974aef55cc5124de213ec30
ntrkiet02 <ntrkiet@gmail.com>
2023-11-28 20:02:29 +0700

    Chỉnh suar lại file khi chay

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,106 +1,180 @@
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}" />
-
+    <meta charset="UTF-8">
+    <meta name="description" content="Videograph Template">
+    <meta name="keywords" content="Videograph, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <!-- CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="{{ asset('public/vendor/font-awesome/css/all.min.css') }}" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" /> 
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Play:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
-    <!-- Scripts -->
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="resources/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="resources/css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="resources/css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="resources/css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="resources/css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="resources/css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="resources/css/style.css" type="text/css">
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
-    @yield('javascript')
 </head>
 
 <body>
-    <div class="container-fluid">
-        <nav class="navbar navbar-expand-md navbar-light shadow-sm bg-info">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="{{ route('frontend') }}">
-                    <i class="fa-light fa-cart-shopping"></i> {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left side of Navbar -->
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('typebeat') }}"><i
-                                    class="fa-light fa-fw fa-diagram-project"></i>Type Beat</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('musician') }}"><i
-                                    class="fa-light fa-fw fa-copyright"></i>Musicians</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('songs') }}"><i class="fa-light fa-fw fa-box"></i>Song</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('status') }}"><i
-                                    class="fa-light fa-fw fa-list-check"></i>Status</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="portflolio"><i
-                                    class="fa-light fa-fw fa-file-invoice"></i>Portflolio</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('user') }}"><i class="fa-light fa-fw fa-users"></i>User</a>
-                        </li>
-                    </ul>
-                    <!-- Right side of Navbar -->
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        @guest
-                        @if (Route::has('login'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}"><i
-                                    class="fa-light fa-fw fa-sign-in-alt"></i> Log in</a>
-                        </li>
-                        @endif
-                        @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}"><i class="fa-light fa-user-plus"></i>
-                                Sign up</a>
-                        </li>
-                        @endif
-                        @else
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#nguoidung" id="navbarDropdown" role="button"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                <i class="fa-light fa-user-circle"></i> {{ Auth::user()->name }}
-                            </a>
-                            <div class="dropdown-menu">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                                    <i class="fa-light fa-fw fa-sign-out-alt"></i> Log out
-                                </a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="post" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                        @endguest
-                    </ul>
+    <!-- Page Preloder -->
+    <!-- Cái củ chuối này là hiển thị quay vòng vòng khi tải trang -->
+    <div id="preloder">
+        <div class="loader"></div>
+    </div>
+    <!-- Header Section Begin -->
+    <header class="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2">
+                    <div class="header__logo">
+                        <a href="#"><img src="resources/img/logo.png" alt=""></a>
+                    </div>
+                </div>
+                <div class="col-lg-10">
+                    <div class="header__nav__option">
+                        <nav class="header__nav__menu mobile-menu">
+                            <ul>
+                                <li class="active"><a href="#">Home</a></li>
+                                <li><a href="#">About</a></li>
+                                <li><a href="#">Portfolio</a></li>
+                                <li><a href="#">Services</a></li>
+                                <li><a href="#">Manager</a>
+                                    <ul class="dropdown">
+                                        <li><a href="{{ route('typebeat') }}">Type Beat</a></li>
+                                        <li><a href="{{ route('musician') }}">Musician</a></li>
+                                        <li><a href="{{ route('songs') }}">Songs</a></li>
+                                        <li><a href="{{ route('status') }}">Status</a></li>
+                                        <li><a href="{{ route('portflolio') }}">Portflolio</a></li>
+                                        <li><a href="{{ route('user') }}">User</a></li>
+
+                                    </ul>
+                                </li>
+                                <li><a href="#">Contact</a></li>
+                                @if (Route::has('login'))
+                                    @auth
+                                    <li><a href="{{ url('/home') }}">Home</a> </li>
+                                    @else
+                                    <li><a href="{{ route('login') }}">Log in</a> </li>
+                                        @if (Route::has('register'))
+                                    <li><a href="{{ route('register') }}">Register</a> </li>
+                                        @endif
+                                    @endauth
+                                </div>
+                            @endif
+                            </ul>
+                        </nav>
+                       
+                    </div>
                 </div>
             </div>
-        </nav>
+            <div id="mobile-menu-wrap"></div>
+        </div>
+    </header> <!-- Header End -->
+    <main>
+    <section class="hero">
+        <div class="hero__slider owl-carousel">
+           <img src = "resources/img/Sakura.jpg"/>
+        </div>
+    </section>
 
-        <main class="pt-3">
-            @yield('content')
-        </main>
 
-        <hr class="shadow-sm" />
-        <footer>Bản quyền &copy; {{ date('Y') }} bởi {{ config('app.name', 'Laravel') }}.</footer>
-    </div>
+    </main>
+    <!-- Footer Section Begin -->
+    <footer class="footer">
+        <div class="container">
+            <div class="footer__top">
+                <div class="row">
+                    <div class="col-lg-6 col-md-6">
+                        <div class="footer__top__logo">
+                            <a href="#"><img src="img/logo.png" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6">
+                        <div class="footer__top__social">
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                            <a href="#"><i class="fa fa-dribbble"></i></a>
+                            <a href="#"><i class="fa fa-instagram"></i></a>
+                            <a href="#"><i class="fa fa-youtube-play"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer__option">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 col-sm-6">
+                        <div class="footer__option__item">
+                            <h5>About us</h5>
+                            <p>&copy; 2024 by Nguyễn Trí Kiệt and Bùi Thị Ngọc Hân. Raptome là trang web siu cấp vip pro không được bất kỳ giải thưởng hay người dùng cũng như feedback nào. Huhu</p>
+                            <a href="#" class="read__more">Read more <span class="arrow_right"></span></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-3">
+                        <div class="footer__option__item">
+                            <h5>Our work</h5>
+                            <ul>
+                                <li><a href="#">Feature</a></li>
+                                <li><a href="#">Latest</a></li>
+                                <li><a href="#">Browse Archive</a></li>
+                                <li><a href="#">Video for web</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-12">
+                        <div class="footer__option__item">
+                            <h5>Newsletter</h5>
+                            <p>Videoprah is an award-winning, full-service production company specializing.</p>
+                            <form action="#">
+                                <input type="text" placeholder="Email">
+                                <button type="submit"><i class="fa fa-send"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer__copyright">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        <p class="footer__copyright__text">Copyright &copy;
+                            <script>
+                                document.write(new Date().getFullYear());
+                            </script>
+                            All rights reserved | This template is made with <i class="fa fa-heart-o"
+                                aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                        </p>
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Footer Section End -->
+
+    <!-- Js Plugins -->
+    <script src="resources/js/jquery-3.3.1.min.js"></script>
+        <script src="resources/js/bootstrap.min.js"></script>
+        <script src="resources/js/jquery.magnific-popup.min.js"></script>
+        <script src="resources/js/mixitup.min.js"></script>
+        <script src="resources/js/masonry.pkgd.min.js"></script>
+        <script src="resources/js/jquery.slicknav.js"></script>
+        <script src="resources/js/owl.carousel.min.js"></script>
+        <script src="resources/js/main.js"></script>
 </body>
 
 </html>
